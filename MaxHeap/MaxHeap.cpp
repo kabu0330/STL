2643bf5d30b6b666f8b5388640036f41f2a83dec
성민 류 <baekhun0330@naver.com>
2025-05-01 16:40:31 +0900

    Heap

---
+++ MaxHeap/MaxHeap.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "MaxHeap.h"
+
+int main()
+{
+	/*			 15
+	*	    10        14
+	*	 8     7    5    9
+	*  3
+	*/
+	UMaxHeap MaxHeap;
+	MaxHeap.Push(7);
+	MaxHeap.Push(10);
+	MaxHeap.Push(5);
+	MaxHeap.Push(3);
+	MaxHeap.Push(14);
+	MaxHeap.Push(15);
+	MaxHeap.Push(9);
+	MaxHeap.Push(8);
+
+	MaxHeap.PreOrder();
+	MaxHeap.PrintArr();
+	                        
+	MaxHeap.Pop();
+	MaxHeap.PrintArr();
+}
