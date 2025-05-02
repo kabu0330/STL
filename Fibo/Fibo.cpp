d9f00b5793357221e923c0b5c5134ecbc69ae11b
성민 류 <baekhun0330@naver.com>
2025-05-02 15:22:47 +0900

    동적계획법

---
+++ Fibo/Fibo.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+
+int Fibo(int _Number)
+{
+	if (_Number < 2)
+	{
+		return _Number;
+	}
+	return Fibo(_Number - 2) + Fibo(_Number - 1);
+}
+
+// 하향식(Top-down) 접근 방법
+std::vector<int> Memo(100, -1);
+int Fibo_Topdown(int _Number)
+{
+	if (_Number < 2)
+	{
+		return _Number;
+	}
+
+	if (Memo[_Number] != -1) // 이미 계산된 적이 있으면
+	{
+		return Memo[_Number];
+	}
+
+	Memo[_Number] = Fibo_Topdown(_Number - 2) + Fibo_Topdown(_Number - 1);
+	return Memo[_Number];
+}
+
+int Fibo_Bottomup(int _Number)
+{
+	std::vector<int> Dp(_Number + 1, 0); // 사용할만큼 모두 0으로 초기화하고 하나씩 값을 채운다.
+	Dp[1] = 1; // 기저 조건
+
+	for (int i = 2; i < _Number; i++)
+	{
+		Dp[i] = Dp[i - 2] + Dp[i - 1];
+	}
+	return Dp[_Number];
+}
+
+int main()
+{
+	// 0 1 1 2 3 5 8 13 21 34 ...
+	std::cout << Fibo(7) << std::endl;
+	std::cout << Fibo_Topdown(7) << std::endl;
+	std::cout << Fibo_Bottomup(7) << std::endl;
+}
+
---
+++ MinimumCostPath/MinimumCostPath.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+
+int solution(const std::vector<std::vector<int>>& _Path)
+{
+    int Rows = static_cast<int>(_Path.size());
+    int Cols = static_cast<int>(_Path[0].size());
+
+    std::vector<std::vector<int>> Matrix(Rows, std::vector<int>(Cols, 0));
+    
+    for (int y = 0; y < Rows; y++)
+    {
+        for (int x = 0; x < Cols; x++)
+        {
+            if (0 == x && 0 == y) // 시작점 (0, 0)이면
+            {
+                Matrix[y][x] = _Path[y][x];
+            }
+            else if (0 == x)
+            {
+                Matrix[y][x] = Matrix[y - 1][x] + _Path[y][x];
+            }
+            else if (0 == y)
+            {
+                Matrix[y][x] = Matrix[y][x - 1] + _Path[y][x];
+            }
+            else
+            {
+                Matrix[y][x] = std::min(Matrix[y - 1][x], Matrix[y][x - 1]) + _Path[y][x];
+            }
+        }
+    }
+    return Matrix[Rows - 1][Cols - 1];
+}
+
+int main()
+{
+    std::vector<std::vector<int>> Path =
+    {
+        {2, 7, 5, 4},
+        {5, 3, 6, 8},
+        {7, 8, 7, 4},
+        {8, 1, 9, 5}
+    };
+    /*  2  9 14 18
+    *   7 10 16 24
+    *  14 18 23 28
+    *  22 19 28 33
+    */ 
+    
+
+    std::cout << solution(Path) << std::endl;
+}
